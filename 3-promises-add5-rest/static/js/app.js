fac7155121af9a25b11eb5c18a596d161fd4ab27
James Tanner <tanner.jc@gmail.com>
2020-05-04 21:26:48 -0400

    add promises example

---
+++ 3-promises-add5-rest/static/js/app.js
@@ -0,0 +1,83 @@
+//https://blog.garstasio.com/you-dont-need-jquery/ajax/#sending-and-receiving-json
+// https://www.youtube.com/watch?v=4EuwgnnXRPI
+
+var data = {
+    numbers: [],
+    addNumber: function(newNumber) {
+        this.numbers.push(newNumber);
+    },
+    getSum: function() {
+        let total = 0;
+        for (i = 0; i < this.numbers.length; i++) {
+            total += this.numbers[i];
+        };
+        return total; 
+    },
+    getSumHTML: function() {
+        let consoleText = "SUM: " + this.getSum().toString();
+        return consoleText;
+    }
+};
+
+/*
+let loadData = function() {
+    $.ajax({
+        url: '/api/data',
+        accept: 'application/json',
+        method: 'GET',
+    }).then(
+        function success(indata) {
+            data.numbers = indata.numbers;
+            document.getElementById("dataconsole").innerHTML = data.getSumHTML();
+            
+        },
+        function fail(data, status) {
+            alert('Request failed.  Returned status of ' + status);
+        }
+    );
+};
+*/
+
+let saveData = function() {
+    $.ajax('/api/data', {
+        method: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({
+            numbers: data.numbers
+        })
+    }).then(
+        function success() {
+            console.log("save successful!");
+        },
+        function fail(data, status) {
+            console.log("save failed!");
+            alert('Request failed.  Returned status of ' + status);   
+        }
+    )
+}
+
+let updateDocument = function() {
+    document.getElementById("dataconsole").innerHTML = data.getSumHTML();
+};
+
+document.getElementById("add5").addEventListener("click", function(){
+    data.addNumber(5);
+    updateDocument();
+});
+
+document.getElementById("save").addEventListener("click", function(){
+    console.log("save clicked!");
+    saveData();
+});
+
+// MAIN
+//loadData();
+fetch('/api/data')
+.then(function(response) {
+    return response.json();
+})
+.then(function(newdata) {
+    data.numbers = newdata.numbers;
+    document.getElementById("dataconsole").innerHTML = data.getSumHTML();
+    console.log("just set the numbers and the text");
+});
